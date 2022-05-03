f8a174b5d406c7b2e502e3a491ec3bf5ba4a9e18
Quentin Wegera <quentin.wegera@mmibordeaux.com>
2022-05-03 17:12:34 +0200

    first commit

---
+++ menu.js
@@ -0,0 +1,16 @@
+document.write('\
+\
+<link rel="stylesheet" href="style/menu.css">\
+    <div class="menu_js">\
+        <a href="index.html">Accueil</a>\
+    \
+        <a href="pages/studies.html">Mes études</a>\
+    \
+        <a href="pages/projects.html">Mes projets</a>\
+    \
+        <a href="pages/passions.html">Mes hobbies</a>\
+    \
+        <a href="pages/contacts.html">Contacts</a>\
+    </div>\
+\
+')
---
+++ menu_pages.js
@@ -0,0 +1,16 @@
+document.write('\
+\
+<link rel="stylesheet" href="../style/menu.css">\
+    <div class="menu_js">\
+        <a href="../index.html">Accueil</a>\
+    \
+        <a href="studies.html">Mes études</a>\
+    \
+        <a href="projects.html">Mes projets</a>\
+    \
+        <a href="passions.html">Mes hobbies</a>\
+    \
+        <a href="contacts.html">Contacts</a>\
+    </div>\
+\
+')
